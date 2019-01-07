0c497e77f238055310b65d14e1bfcc0311367342
Junglelin <695562236@qq.com>
2019-01-07 23:20:06 +0900

    daily

--- app/src/main/java/com/nufter/nufterdictionary/MainActivity.kt
+++ app/src/main/java/com/nufter/nufterdictionary/MainActivity.kt
@@ -3,7 +3,9 @@ package com.nufter.nufterdictionary
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        Log.d("logout", "on create execute")
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
